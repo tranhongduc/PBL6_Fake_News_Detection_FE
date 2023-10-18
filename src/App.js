2b501a7d8fa722e931f8e960620991c8920e5252
ductran <93823088+tranhongduc@users.noreply.github.com>
2023-10-18 14:12:58 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React from "react"
-import { Footer } from "./components/footer/Footer"
-import { Header } from "./components/header/Header"
 import { Home } from "./pages/home/Home"
-import { Login } from "./pages/login/Login"
-import { Regsiter } from "./pages/login/Regsiter"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { DetailsPages } from "./pages/details/DetailsPages"
 import { Account } from "./pages/account/Account"
 import { Create } from "./components/create/Create"
+import Default from "./pages/Default"
+import AdminPage from "./pages/admin/admin"
+import Team from "./pages/admin/scenes/team"
+import Contacts from "./pages/admin/scenes/contacts"
+import Invoices from "./pages/admin/scenes/invoices"
+import Bar from "./pages/admin/scenes/bar"
+import Pie from "./pages/admin/scenes/pie"
+import Line from "./pages/admin/scenes/line"
+import FAQ from "./pages/admin/scenes/faq"
+import Geography from "./pages/admin/scenes/geography"
+import Dashboard from "./pages/admin/scenes/dashboard"
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Regsiter} />
-          <Route exact path='/details/:id' component={DetailsPages} />
-          <Route exact path='/account' component={Account} />
-          <Route exact path='/create' component={Create} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
+        <Routes>
+        <Route path='/' element={<Default/>} >
+          <Route index element={<Home/>} />
+          <Route path='details/:id' element={<DetailsPages/>} />
+          <Route path='account' element={<Account/>} />
+          <Route path='create' element={<Create/>} />
+        </Route>
+        <Route path='admin' element={<AdminPage/>} >
+          <Route index element={<Dashboard />} />
+          <Route path="team" element={<Team />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="bar" element={<Bar />} />
+          <Route path="pie" element={<Pie />} />
+          <Route path="line" element={<Line />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="geography" element={<Geography />} />
+        </Route>
+        </Routes>
   )
 }
 export default App
